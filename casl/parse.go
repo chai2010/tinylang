d6842457284def99cab8d08f9c52b031e02f99f4
chai2010 <chaishushan@gmail.com>
2019-11-10 22:51:52 +0800

    zz

---
+++ casl/parse.go
@@ -0,0 +1,10 @@
+// Copyright 2019 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+// 解析CASL程序, 返回机器码和对应的可读格式指令
+func ParseCASL(code string) (prog []uint16, ins []string, err error) {
+	panic("TODO")
+}
